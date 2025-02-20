bc49bcc3bf52ff2b0bcbd303920617d4c8bd351c
PaulineAquino <Aquinopauline29@gmail.com>
2025-02-20 20:15:30 +0800

    Submitting Python Week 04 - Laboratory # 08

---
+++ if_statement_basic.py
@@ -0,0 +1,9 @@
+# Define a variable
+temperature = 35  # Or any other number
+
+# Write an if statement
+if temperature > 30:
+
+# Print a message (indented)
+    print("It's a hot day!")  # Indented 4 spaces
+
